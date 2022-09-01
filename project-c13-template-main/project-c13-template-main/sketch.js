4476ad427dfee51485b06725df6c6b3cb9823257
Vini-V-Daudt <111530852+Vini-V-Daudt@users.noreply.github.com>
2022-09-01 15:02:30 -0300

    Update sketch.js

--- project-c13-template-main/project-c13-template-main/sketch.js
+++ project-c13-template-main/project-c13-template-main/sketch.js
@@ -56,18 +56,18 @@ function createApples(){
   apple.lifetime = 150;
 }
 
-  function createApples(){
-    apple = createSprite(random(50, 350) , 40, 10, 10);
-    apple.addImage(appleImg);
-    apple.scale=0.07;
-    apple.velocityY = 3;
-    apple.lifetime = 150;
+  function createOrange(){
+    orange = createSprite(random(50, 350) , 40, 10, 10);
+    orange.addImage(appleImg);
+    orange.scale=0.08;
+    orange.velocityY = 3;
+    orange.lifetime = 150;
   }
 
-  function createApples(){
-    apple = createSprite(random(50, 350) , 40, 10, 10);
-    apple.addImage(appleImg);
-    apple.scale=0.07;
-    apple.velocityY = 3;
-    apple.lifetime = 150;
+  function createRed(){
+    red = createSprite(random(50, 350) , 40, 10, 10);
+    red.addImage(appleImg);
+    red.scale=0.06;
+    red.velocityY = 3;
+    red.lifetime = 150;
   }
